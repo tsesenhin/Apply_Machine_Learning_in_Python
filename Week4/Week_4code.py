399d3497e090bd287db0a931f3fa5a8669479f3e
sen hin tse <senhintse@sens-mbp.home>
2020-05-07 16:51:57 +0100

    07-05-20

--- Week4/Week_4code.py
+++ Week4/Week_4code.py
@@ -406,3 +406,30 @@ for thisaxisrow, thisactivation in zip(subaxes, ['tanh', 'relu']):
         thisaxis.set_title('MLP regression\nalpha={}, activation={})'
                           .format(thisalpha, thisactivation))
         plt.tight_layout()
+
+
+
+
+
+# application to real-world dataset for classification
+
+
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import MinMaxScaler
+
+
+scaler = MinMaxScaler() # Normailisation is needed for regression or linear model usually
+
+X_train, X_test, y_train, y_test = train_test_split(X_cancer, y_cancer, random_state = 0)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+clf = MLPClassifier(hidden_layer_sizes = [100, 100], alpha = 5.0,
+                   random_state = 0, solver='lbfgs').fit(X_train_scaled, y_train)
+
+print('Breast cancer dataset')
+print('Accuracy of NN classifier on training set: {:.2f}'
+     .format(clf.score(X_train_scaled, y_train)))
+print('Accuracy of NN classifier on test set: {:.2f}'
+     .format(clf.score(X_test_scaled, y_test)))
