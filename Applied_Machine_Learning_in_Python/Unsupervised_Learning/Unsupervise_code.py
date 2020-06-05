b10960554ebc7426eb03d99292984858fe6fa836
sen hin tse <senhintse@sens-MacBook-Pro.local>
2020-06-05 15:56:07 +0100

    05-05-20

--- Applied_Machine_Learning_in_Python/Unsupervised_Learning/Unsupervise_code.py
+++ Applied_Machine_Learning_in_Python/Unsupervised_Learning/Unsupervise_code.py
@@ -155,3 +155,110 @@ plot_labelled_scatter(X_tsne, y_cancer,
 plt.xlabel('First t-SNE feature')
 plt.ylabel('Second t-SNE feature')
 plt.title('Breast cancer dataset t-SNE');
+
+
+
+
+
+
+
+
+
+
+## Clustering
+
+# K-means
+
+#This example from the lecture video creates an artificial dataset with make_blobs, then applies k-means to find 3 clusters, and plots the points in each cluster identified by a corresponding color.
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from adspy_shared_utilities2 import plot_labelled_scatter
+
+X, y = make_blobs(random_state = 10)
+
+kmeans = KMeans(n_clusters = 3) # creation of 3 clusters
+kmeans.fit(X)
+
+plot_labelled_scatter(X, kmeans.labels_, ['Cluster 1', 'Cluster 2', 'Cluster 3'])
+
+
+
+
+#Example showing k-means used to find 4 clusters in the fruits dataset.  Note that in general, it's important to scale the individual features before applying k-means clustering.
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from adspy_shared_utilities2 import plot_labelled_scatter
+from sklearn.preprocessing import MinMaxScaler
+
+fruits = pd.read_table('fruit_data_with_colors.txt')
+X_fruits = fruits[['mass','width','height', 'color_score']].values
+y_fruits = fruits[['fruit_label']] - 1
+
+X_fruits_normalized = MinMaxScaler().fit(X_fruits).transform(X_fruits)  
+
+kmeans = KMeans(n_clusters = 4, random_state = 0)
+kmeans.fit(X_fruits_normalized)
+
+plot_labelled_scatter(X_fruits_normalized, kmeans.labels_, 
+                      ['Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4'])
+
+
+
+
+
+# Agglomerative clustering
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import AgglomerativeClustering
+from adspy_shared_utilities2 import plot_labelled_scatter
+
+X, y = make_blobs(random_state = 10)
+
+cls = AgglomerativeClustering(n_clusters = 3) # this means when formed 3 clusters, stop !
+cls_assignment = cls.fit_predict(X)
+
+plot_labelled_scatter(X, cls_assignment, 
+        ['Cluster 1', 'Cluster 2', 'Cluster 3'])
+
+
+#### Creating a dendrogram (using scipy)
+
+### Creating a dendrogram (using scipy)
+#This dendrogram plot is based on the dataset created in the previous step with make_blobs, but for clarity, only 10 samples have been selected for this example, as plotted here:
+
+X, y = make_blobs(random_state = 10, n_samples = 10)
+plot_labelled_scatter(X, y, 
+        ['Cluster 1', 'Cluster 2', 'Cluster 3'])
+print(X)
+
+
+#And here's the dendrogram corresponding to agglomerative clustering of the 10 points above using Ward's method.  The index 0..9 of the points corresponds to the index of the points in the X array above.  For example, point 0 (5.69, -9.47) and point 9 (5.43, -9.76) are the closest two points and are clustered first.
+
+from scipy.cluster.hierarchy import ward, dendrogram
+plt.figure()
+dendrogram(ward(X))
+plt.show()
+
+
+
+
+
+
+
+
+### DBSCAN clustering
+
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(random_state = 9, n_samples = 25)
+
+dbscan = DBSCAN(eps = 2, min_samples = 2)
+
+cls = dbscan.fit_predict(X)
+print("Cluster membership values:\n{}".format(cls))
+
+plot_labelled_scatter(X, cls + 1, 
+        ['Noise', 'Cluster 0', 'Cluster 1', 'Cluster 2'])
