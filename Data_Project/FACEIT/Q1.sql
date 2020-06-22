431b1eb8a682b785de6bc95100ceffc4b5d5eeb1
sen hin tse <senhintse@sens-MacBook-Pro.local>
2020-06-22 13:36:01 +0100

    22-06-20

--- Data_Project/FACEIT/Q1.sql
+++ Data_Project/FACEIT/Q1.sql
@@ -22,3 +22,21 @@ group by user_id, match_id, game, created_at, membership, faction, winner) as ta
 where rank1 >= 3
 
 
+
+
+
+select 
+
+q.query_name,
+
+round(avg((cast(rating as float) /cast(position as float))),2) AS quality
+,
+
+round(cast(sum(case when q.rating < 3 then 1 else 0 end) as float)/cast(count(q.rating) as float)*100,2)
+as poor_query_percentage
+
+from queries AS q 
+
+group by q.query_name
+
+
